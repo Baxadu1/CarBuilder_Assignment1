9acea198c38e0afdda064f35ef13fe612f5b02aa
baqyt kyanysh <bakyt.kuanysh6@gmail.com>
2025-09-14 15:02:18 +0500

    created private constructor Car with Builder

--- src/Car.java
+++ src/Car.java
@@ -3,4 +3,44 @@ public class Car {
     private final String engine;
     private final boolean hasGPS;
     private final boolean hasTripComputer;
+
+    private Car(Builder builder) {
+        this.seats = builder.seats;
+        this.engine = builder.engine;
+        this.hasGPS = builder.hasGPS;
+        this.hasTripComputer = builder.hasTripComputer;
+    }
+    public String toString() {
+        return "Car [Seats=" + seats +
+                ", Engine=" + engine +
+                ", GPS=" + hasGPS +
+                ", TripComputer=" + hasTripComputer + "]";
+    }
+
+    public static class Builder {
+        private int seats;
+        private String engine;
+        private boolean hasGPS;
+        private boolean hasTripComputer;
+
+        public Builder setSeats(int seats) {
+            this.seats = seats;
+            return this;
+        }
+
+        public Builder setEngine(String engine) {
+            this.engine = engine;
+            return this;
+        }
+
+        public Builder setGPS(boolean gps) {
+            this.hasGPS = gps;
+            return this;
+        }
+
+        public Builder setTripComputer(boolean tripComputer) {
+            this.hasTripComputer = tripComputer;
+            return this;
+        }
+
 }
